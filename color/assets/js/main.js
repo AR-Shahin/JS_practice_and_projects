d94980524410eebf3f48713e8153a6bc19775a4c
AR-Shahin <mdshahinmije96@gmail.com>
2021-01-18 11:04:11 +0600

    work on color

---
+++ color/assets/js/main.js
@@ -0,0 +1,29 @@
+const colors = ['red','blue','green','yellow','pink','coral','purple']
+const print = (d)=>{
+    console.log(d);
+}
+const mainDiv = document.querySelector('#mainDiv')
+const h4 = document.querySelector('h4')
+
+const selectedColor =  document.querySelector('#selectedColor')
+h4.className = 'mt-2'
+h4.style.display = 'inline-block'
+
+
+for (let color of colors){
+    const box = document.createElement('div')
+    box.classList.add('box')
+    box.style.background = color
+    mainDiv.appendChild(box)
+    box.addEventListener('click',function(){
+        h4.innerHTML = 'You have selected -> '
+        selectedColor.style.background = color
+        selectedColor.innerHTML = color.toUpperCase()
+    })
+
+
+}
+
+
+var x = typeof colors;
+print(x)
