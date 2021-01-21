cc4b7ec31e76c8d6474be5fdd64034ff0360ab6c
AR-Shahin <mdshahinmije96@gmail.com>
2021-01-21 18:59:07 +0600

    animate heading

---
+++ typeing _heading/assets/js/main.js
@@ -0,0 +1,26 @@
+let $ = (data) => document.querySelector(data);
+let print = (data) => console.log(data)
+
+
+const texts = ['photographer','designer','web developer']
+
+let count = 0
+let index = 0
+let currentText = ''
+let letter = '';
+
+
+(function type(){
+    if(count === texts.length){
+        count = 0
+    }
+    currentText = texts[count]
+    letter = currentText.slice(0,++index)
+   // print(letter + index)
+    $('#dynamicText').innerHTML = letter
+    if(letter.length === currentText.length){
+        count ++;
+        index = 0;
+    }
+    setTimeout(type,400)
+})()
