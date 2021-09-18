8bc499f1ea1485b8b7ce48ae61487952001439dc
AR Shahin <mdshahinmije96@gmail.com>
2021-09-18 20:50:13 +0600

    counter client

--- DOM/Count-Client/assets/app.js
+++ DOM/Count-Client/assets/app.js
@@ -10,17 +10,49 @@ const _ = (el) => document.createElement(el);
 
 let targets = _$('h4')
 let [first,second,third] = targets
-log(first.dataset.count)
-
-window.addEventListener('load',function(){
-    let count = 0;
-    while(count!= first.dataset.count){
-        first.textContent = count;
-        count ++
-    }
-})
+let count = 1;
+let count2 = 1;
+let count3 = 1;
+// window.addEventListener('load',function(){
+ 
+//     while(count!= first.dataset.count){
+//         first.textContent = count;
+//         count ++
+//     }
+// })
 
 
 const firstCol = ()=>{
-    
+    first.textContent = ++ count;
+    if(count == first.dataset.count){
+        clearInterval(colOne);
+    }
 }
+const secondCol = ()=>{
+    second.textContent = ++ count2;
+    if(count2 == second.dataset.count){
+        clearInterval(colTwo);
+    }
+}
+const thirdCol = ()=>{
+    third.textContent = ++ count3;
+    if(count3 == third.dataset.count){
+        clearInterval(colThree);
+    }
+}
+let colOne= setInterval(firstCol,80);
+let colTwo= setInterval(secondCol,80);
+let colThree= setInterval(thirdCol,80);
+
+
+let counterSection = $('#counter');
+let x = counterSection.getBoundingClientRect().top;
+
+// log(x.bottom)
+// log(x.top)
+window.addEventListener('scroll',function(){
+     log(this.innerHeight)
+    if(x === window.innerHeight){
+        this.alert(5)
+    }
+})
